1327226efb3246f64f37c3d30c073e828e089d6f
Shishir Jha <shishir230501@gmail.com>
2023-10-16 21:48:22 +0530

    MVC architecture setup done

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
@@ -13,6 +14,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
